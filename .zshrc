d5cca3fd9e5d1b55dc8a3544c79dc7cd35558d7f
William Helmrath <45471376+williamhelmrath@users.noreply.github.com>
2025-09-04 13:56:11 -0400

    install evalcache plugin

--- .zshrc
+++ .zshrc
@@ -10,13 +10,11 @@ export ZSH="$HOME/.oh-my-zsh"
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 ZSH_THEME="robbyrussell"
 
-export NVM_LAZY_LOAD=true
-
 # Which plugins would you like to load?
 # Standard plugins can be found in $ZSH/plugins/
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(evalcache kubectl git zsh-nvm)
+plugins=(evalcache kubectl git)
 
 source $ZSH/oh-my-zsh.sh
--- install.sh
+++ install.sh
@@ -58,11 +58,16 @@ fi
 # Install Oh My Zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Installing Oh My Zsh..."
-#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/evalcache" ]; then
+    echo "Installing evalcache plugin..."
+    git clone https://github.com/mroth/evalcache ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/evalcache
 fi
 
 # Set Zsh as default shell
 if [ "$SHELL" != "$(which zsh)" ]; then
   echo "Setting Zsh as default shell..."
-#   chsh -s $(which zsh)
+  chsh -s $(which zsh)
 fi
